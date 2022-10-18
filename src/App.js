2dda6d6cfe34006303c3bd3a8dccf574fb120c29
Mhamzahhameed <hamzahraja11@gmail.com>
2022-10-18 12:55:59 +0500

    Basic Todo App
    created a very basic todo app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,116 @@
-import logo from './logo.svg';
-import './App.css';
+import { useCallback, useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [task, setTask] = useState("");
+  const [arr, setArr] = useState([]);
+  const [searchTerm, setSearchTerm] = useState([]);
+  // const [preArr, setPreArr] = useState([]);
+
+  // Function to push in Array
+  const addTask = () => {
+    if (task?.length) {
+      setArr([...arr, task]);
+      setTask("");
+    } else {
+      alert("Please enter your task");
+    }
+  };
+  // const savePreArr = useCallback(() => {
+  //   setPreArr(arr);
+  // }, []);
+
+  const filterTasks = (e) => {
+    if (e.target.value) {
+      setSearchTerm(
+        arr.filter((a) => {
+          return a.match(e.target.value);
+        })
+      );
+      // setArr(searchTerm);
+    } else {
+      setSearchTerm([]);
+      // setArr(preArr);
+    }
+  };
+  // Function to delete specific task
+  const deleteTask = useCallback(
+    (ind) => {
+      arr?.splice(ind, 1);
+      setArr([...arr]);
+    },
+    [arr]
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <input
+        type='search'
+        placeholder='Search'
+        id='SearchInput'
+        onChange={filterTasks}
+      />
+      <br />
+      <br />
+      <ul>
+        {searchTerm?.map((a, i) => {
+          return (
+            <li
+              key={i}
+              style={{
+                marginTop: "3px",
+                listStyleType: "none",
+              }}
+            >
+              <p style={{ fontFamily: "-moz-initial" }}>
+                <b>{a}</b>
+              </p>
+            </li>
+          );
+        })}
+      </ul>
+      <h1>Todo App</h1>
+      <br />
+      <input
+        type='text'
+        name='task'
+        onChange={(e) => setTask(e?.target?.value)}
+        placeholder='Please enter the task here'
+        value={task}
+      />{" "}
+      {/* Button to add current task in Array */}
+      <button onClick={addTask} className='btn btn-primary'>
+        Add task
+      </button>
+      <br />
+      <br />
+      {/* Displaying values of Array one by one */}
+      <ol>
+        {arr?.map((a, i) => {
+          return (
+            <li
+              key={i}
+              style={{
+                marginTop: "3px",
+                listStylePosition: "inside",
+              }}
+            >
+              <span style={{ fontFamily: "-moz-initial" }}>
+                <b>{a}</b>
+              </span>{" "}
+              <button
+                type='button'
+                className='btn btn-danger'
+                onClick={() => deleteTask(i)}
+              >
+                Delete
+              </button>
+            </li>
+          );
+        })}
+      </ol>
     </div>
   );
-}
+};
 
 export default App;
